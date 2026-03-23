b467bfd6be2cf0b75752d4cf11a659f95c658a8b
michael <michael.g.george@gmail.com>
2026-03-23 08:18:24 +0530

    test(cli): add basic scan integration test

--- cli/tests/common/assertions.rs
+++ cli/tests/common/assertions.rs
@@ -1,9 +1,11 @@
 use std::process::Output;
 
+#[allow(dead_code)]
 pub fn assert_success(output: &Output) {
     assert!(output.status.success());
 }
 
+#[allow(dead_code)]
 pub fn assert_failure(output: &Output) {
     assert!(!output.status.success());
 }
