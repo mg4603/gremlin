79fd5d74cb636faf1d6ba5946b66ec20de8ad406
michael <michael.g.george@gmail.com>
2026-03-23 08:14:53 +0530

    test(cli): add spawn_cli helper

--- cli/tests/common/process.rs
+++ cli/tests/common/process.rs
@@ -1,5 +1,15 @@
-use std::process::{Command, Output};
+use std::process::{Child, Command, Output, Stdio};
 
+#[allow(dead_code)]
+pub fn spawn_cli(args: &[&str], stdout: Stdio) -> Child {
+    Command::new(env!("CARGO_BIN_EXE_cli"))
+        .args(args)
+        .stdout(stdout)
+        .spawn()
+        .expect("failed to spawn cli")
+}
+
+#[allow(dead_code)]
 pub fn run_cli(args: &[&str]) -> Output {
     Command::new(env!("CARGO_BIN_EXE_cli"))
         .args(args)
